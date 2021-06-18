fb976746975ed0fc9cb24b9ec4cc8ffccc4f4269
huzejun <huzejun1990>
2021-06-19 02:26:38 +0800

    springboot2.2.1整合 redis

---
+++ redis_code/redis_springboot2021/src/main/java/com/dream/redis_springboot2021/RedisSpringboot2021Application.java
@@ -0,0 +1,13 @@
+package com.dream.redis_springboot2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringboot2021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSpringboot2021Application.class, args);
+    }
+
+}
